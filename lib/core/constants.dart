27fa83842475c29ac3cb7bde4bb7c931f9f81541
liby <1412416598@qq,com>
2025-09-17 16:20:14 +0800

    update

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,9 +1,11 @@
 //常量定义
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConstants {
-  static const String baseUrl = 'https://mh59fby3va.re.qweatherapi.com/v7';
-  static const String geoApiUrl =
-      'https://mh59fby3va.re.qweatherapi.com/geo/v2';
-  static const String apikey = '923757dbb0534dedabba25c6e2c25835';
+  static String get baseUrl => dotenv.env['API_BASE_URL'] ?? '';
+  static String get geoApiUrl => dotenv.env['GEO_API_URL'] ?? '';
+  static String get apikey => dotenv.env['API_KEY'] ?? '';
 
   static const String weatherNow = '/weather/now';
   static const String cityLookup = '/city/lookup';
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'viewmodel/weather_viewmodel.dart';
 import 'view/weather_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
